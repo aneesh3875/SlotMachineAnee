d4cdc5952f9ec9e9f4c27eba862af678153d744b
aneeshdasari <ux.aneeshdasari@gmail.com>
2023-04-14 18:27:05 +0530

    Initial Commit

---
+++ SlotMachineAnee/SlotMachineAneeApp.swift
@@ -0,0 +1,17 @@
+//
+//  SlotMachineAneeApp.swift
+//  SlotMachineAnee
+//
+//  Created by Aneesh on 14/04/23.
+//
+
+import SwiftUI
+
+@main
+struct SlotMachineAneeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
